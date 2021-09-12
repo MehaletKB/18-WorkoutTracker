92c7426d98beb68e3eeb11d1501510aef982f343
MehaletKB <mkesatebirhan@gmail.com>
2021-09-12 15:41:32 -0500

    :construction: view route setup

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ mongoose.connect('mongodb://localhost/workout', {
   useUnifiedTopology: true,
 });
 
+app.use(require("./routes/views.js"))
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });

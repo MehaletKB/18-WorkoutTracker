80bbd7a262bff44baca6945adbb079092ac2fdf4
MehaletKB <mkesatebirhan@gmail.com>
2021-09-16 17:37:58 -0500

    :construction: server js edit

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 const router = require('./routes/apiRoutes.js');
 const views = require('./routes/views.js');
 
-const mongoose = require('mongoose');
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(logger('dev'));
 
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public", { extensions: ["html"] }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(  process.env.DB_CLIENT_URL || "mongodb://localhost/workout", {
+mongoose.connect(process.env.DB_CLIENT_URL || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 app.use(router);
